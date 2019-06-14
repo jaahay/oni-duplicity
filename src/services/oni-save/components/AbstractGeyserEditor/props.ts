4edfabd159f83de5f31e52f5a47ba56f11e8560b
James Hay <jaahay@users.noreply.github.com>
2019-06-13 22:57:07 -0700

    Enable additional geyser timing configurations
    Add sliders for more emission controls

--- src/services/oni-save/components/AbstractGeyserEditor/props.ts
+++ src/services/oni-save/components/AbstractGeyserEditor/props.ts
@@ -5,6 +5,14 @@ export interface AbstractGeyserEditorProps {
 export interface AbstractGeyserEditorRenderProps {
   geyserType: string | null;
   emitRate: number | null;
+  emitIterationLength: number | null;
+  emitIterationPercent: number | null;
+  emitYearLength: number | null;
+  emitYearPercent: number | null;
   onChangeEmitRate(value: number): void;
   onChangeGeyserType(type: string): void;
+  onChangeEmitIterationLength(value: number): void;
+  onChangeEmitIterationPercent(value: number): void;
+  onChangeEmitYearLength(value: number): void;
+  onChangeEmitYearPercent(value: number): void;
 }
